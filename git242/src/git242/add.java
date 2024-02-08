efba4ca8df847f227ad5155244d9d3b2ef117242
prathameshshyamrane <psr261004@gmail.com>
2024-02-08 17:54:03 +0530

    commit

---
+++ git242/src/git242/add.java
@@ -0,0 +1,23 @@
+package git242;
+
+import java.util.Scanner;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,y,sum;
+		System.out.println("Enter two numbers");
+		Scanner scan=new Scanner(System.in);
+		a=scan.nextInt();
+		y=scan.nextInt();
+		sum=a+y;
+		System.out.println("The sum is : "+sum);
+
+	}
+
+		// TODO Auto-generated method stub
+
+	}
+
+
